8839fd7e7d39e28b66585e3b893120a158a7077d
richuak <richuakuttikattu@gmail.com>
2023-07-27 13:54:27 +0530

    calling functions of other contracts, no interface

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -5,6 +5,7 @@ pragma solidity 0.8.19;
 
 //imports only the specified contract(s) from the imported file
 //much better control over what's being impored, instead of a blind import of the whole file.
+//called 'named import'
 import {SimpleStorage, NotUsedContract} from "./SimpleStorage.sol";
 
 contract StorageFactory {
@@ -22,5 +23,18 @@ contract StorageFactory {
         // declaring and assigning the variable in one go:
         SimpleStorage simpleStorage = new SimpleStorage();
         listOfSimpleStorage.push(simpleStorage);
+    }
+
+    //calling a function of another contract from this contract
+    //since the whole other contract was imported, it has the ABI already
+    function sfStore(uint256 _index, uint256 _numberToStore) public {
+        //initiating simpleStage like this is a bit longform
+        SimpleStorage simpleStorage = listOfSimpleStorage[_index];
+        simpleStorage.store(_numberToStore);
+    }
+
+    function sfRead(uint256 _index) public view returns (uint256) {
+        //the short form of directly calling the method since we know the list contains SimpleStorage datatypes
+        return listOfSimpleStorage[_index].retrieve();
     }
 }
