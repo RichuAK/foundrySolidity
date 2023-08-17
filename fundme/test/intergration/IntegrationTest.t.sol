57057d3023bd916281163a35fd50ada1edd26740
richuak <richuakuttikattu@gmail.com>
2023-08-17 10:31:46 +0530

    Intergration Test init

---
+++ fundme/test/intergration/IntegrationTest.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+import {FundFundMe, WithdrawFundMe} from "../../script/Interactions.s.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
+
+contract Integration is StdCheats, Test {}
