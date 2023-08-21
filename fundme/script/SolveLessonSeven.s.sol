f138d508d6910c1c3092e7889c18aa27f1986ad9
richuak <richuakuttikattu@gmail.com>
2023-08-21 22:34:55 +0530

    integration failed: non ERC721Receiver implementer

--- fundme/script/SolveLessonSeven.s.sol
+++ fundme/script/SolveLessonSeven.s.sol
@@ -3,13 +3,31 @@ pragma solidity ^0.8.20;
 
 import {Script, console} from "forge-std/Script.sol";
 
-contract GetPrivateData is Script {
-    function run() external view {
-        address LessonSeven = address(
-            0xD7D127991c6A89Df752FC3daeC17540aE8B86101
+contract LessonSeven {
+    function solveChallenge(
+        uint256 valueAtStorageLocationSevenSevenSeven,
+        string memory yourTwitterHandle
+    ) external {}
+}
+
+contract SolveLessonSeven is Script {
+    address LessonSevenAddress =
+        address(0xD7D127991c6A89Df752FC3daeC17540aE8B86101);
+
+    function run() external {
+        uint256 privateData = getPrivateData();
+
+        LessonSeven lessonSeven = LessonSeven(LessonSevenAddress);
+        lessonSeven.solveChallenge(privateData, "richuak1");
+    }
+
+    function getPrivateData() internal view returns (uint256) {
+        bytes32 privateVariable = vm.load(
+            LessonSevenAddress,
+            bytes32(uint256(777))
         );
-        bytes32 leet = vm.load(LessonSeven, bytes32(uint256(777)));
         console.log("Attempting to find Solution for Lesson Seven");
-        console.log("The private data: %s", uint256(leet)); // 1337
+        console.log("The private data: %s", uint256(privateVariable));
+        return uint256(privateVariable);
     }
 }
