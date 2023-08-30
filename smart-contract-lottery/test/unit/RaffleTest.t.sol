66a4df42d210fcfbf3bb4c7763388424cfba9e37
richuak <richuakuttikattu@gmail.com>
2023-08-30 21:30:58 +0530

    player gets added and event emitted

--- smart-contract-lottery/test/unit/RaffleTest.t.sol
+++ smart-contract-lottery/test/unit/RaffleTest.t.sol
@@ -11,6 +11,9 @@ contract RaffleTest is Test {
     Raffle raffle;
     HelperConfig helperConfig;
 
+    /**Event Declarations */
+    event EnteredRaffle(address indexed player);
+
     uint256 entranceFee;
     uint256 interval;
     address vrfCoordinator;
@@ -40,7 +43,7 @@ contract RaffleTest is Test {
     }
 
     // entering raffle test
-    function testEnterRaffleRevertWhenNotPaid() public {
+    function testEnterRaffleRevertsWhenNotPaidEnough() public {
         // Arrange
         uint256 lessThanEntranceFee = raffle.getEntranceFee() - 100;
         // console.log("Entrance fee:", raffle.getEntranceFee());
@@ -48,5 +51,33 @@ contract RaffleTest is Test {
         // Act / Assert
         vm.expectRevert(Raffle.Raffle__NotEnoughEthSent.selector);
         raffle.enterRaffle{value: lessThanEntranceFee}();
+    }
+
+    function testPlayerGetsAddedToPlayers() public {
+        // Arrange
+        uint256 initPlayersLength = raffle.getPlayersSize();
+        vm.prank(PLAYER);
+        // Act
+        raffle.enterRaffle{value: entranceFee}();
+        // Assert
+        assertEq(PLAYER, raffle.getPlayerAtIndex(initPlayersLength));
+    }
+
+    /* Events a bit tricky to test in foundry. 
+
+    expectemit cheatcode: https://book.getfoundry.sh/cheatcodes/expect-emit?highlight=expectemit#expectemit
+    You do expect emit, then you emit the event in your test itself, and then you do the transaction that does the event emit.
+    
+    which means you have to declare the event outside the functions in the test file.
+
+    */
+
+    function testEnteredRaffleEventIsEmitted() public {
+        vm.prank(PLAYER);
+        // the address paremeter/property is not exactly strongly binded(?)
+        vm.expectEmit(true, false, false, false, address(raffle));
+        // vm.expectEmit(true, false, false, false);
+        emit EnteredRaffle(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
     }
 }
