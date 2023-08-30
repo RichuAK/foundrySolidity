dc25bca8fe85676edc196a5043cac2e80ce726ad
richuak <richuakuttikattu@gmail.com>
2023-08-30 16:15:00 +0530

    testRaffleState v1

--- smart-contract-lottery/test/unit/RaffleTest.t.sol
+++ smart-contract-lottery/test/unit/RaffleTest.t.sol
@@ -5,16 +5,22 @@ pragma solidity ^0.8.21;
 import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
 import {Raffle} from "../../src/Raffle.sol";
 import {Test, console} from "forge-std/Test.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
 
 contract RaffleTest is Test {
     Raffle raffle;
+    HelperConfig helperConfig;
 
     address PLAYER = makeAddr("player");
     uint256 public constant STARTING_BALANCE = 1 ether;
 
     function setUp() external {
         DeployRaffle deployRaffle = new DeployRaffle();
-        raffle = deployRaffle.run();
+        (raffle, helperConfig) = deployRaffle.run();
         vm.deal(PLAYER, STARTING_BALANCE);
+    }
+
+    function testRaffleState() public view {
+        assert(raffle.getRaffleState() == Raffle.RaffleState.OPEN);
     }
 }
