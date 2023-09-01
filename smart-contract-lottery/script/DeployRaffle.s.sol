b706d098fb57fc0163b5851bee4b097e2a6c52bd
richuak <richuakuttikattu@gmail.com>
2023-09-01 12:57:17 +0530

    funding subscription and adding raffle as consumer

--- smart-contract-lottery/script/DeployRaffle.s.sol
+++ smart-contract-lottery/script/DeployRaffle.s.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.21;
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
-import {CreateSubscription} from "./Interactions.s.sol";
+import {CreateSubscription, FundSubscription, AddConsumer} from "./Interactions.s.sol";
 
 contract DeployRaffle is Script {
     function run() external returns (Raffle, HelperConfig) {
@@ -17,13 +17,20 @@ contract DeployRaffle is Script {
             bytes32 gasLane,
             uint64 subscriptionId,
             uint32 callBackGasLimit,
-
+            address linkToken
         ) = helperConfig.activeNetworkConfig();
 
         if (subscriptionId == 0) {
             CreateSubscription createSubscription = new CreateSubscription();
             subscriptionId = createSubscription.createSubscription(
                 vrfCoordinator
+            );
+
+            FundSubscription fundSubscription = new FundSubscription();
+            fundSubscription.fundSubscription(
+                vrfCoordinator,
+                subscriptionId,
+                linkToken
             );
         }
 
@@ -37,6 +44,13 @@ contract DeployRaffle is Script {
             callBackGasLimit
         );
         vm.stopBroadcast();
+
+        AddConsumer addConsumer = new AddConsumer();
+        addConsumer.addConsumer(
+            address(raffle),
+            vrfCoordinator,
+            subscriptionId
+        );
         return (raffle, helperConfig);
     }
 }
