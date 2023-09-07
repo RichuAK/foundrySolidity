d60676a95b384c7ef805ed80f1a8fbb38c8df052
richuak <richuakuttikattu@gmail.com>
2023-09-07 20:00:40 +0530

    Neapolitan NFT

---
+++ nft/src/NeapolitanNFT.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.21;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NeapolitanNFT is ERC721 {
+    uint256 private s_counter;
+
+    constructor() ERC721("Neapolitan Novels", "NNT") {
+        s_counter = 0;
+    }
+}
