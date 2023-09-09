850edbc1cc5369e624a6253a4242abf76a33f4fc
richuak <richuakuttikattu@gmail.com>
2023-09-09 20:17:21 +0530

    Sample NFT init

--- nft/src/NeapolitanNFT.sol
+++ nft/src/NeapolitanNFT.sol
@@ -5,9 +5,9 @@ pragma solidity ^0.8.21;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NeapolitanNFT is ERC721 {
-    uint256 private s_counter;
+    uint256 private s_tokenCounter;
 
     constructor() ERC721("Neapolitan Novels", "NNT") {
-        s_counter = 0;
+        s_tokenCounter = 0;
     }
 }
