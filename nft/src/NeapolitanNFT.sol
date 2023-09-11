9ec6036543823195521c261be3cd80ed51d2e39a
richuak <richuakuttikattu@gmail.com>
2023-09-11 16:23:41 +0530

    mint with any tokenUri

--- nft/src/NeapolitanNFT.sol
+++ nft/src/NeapolitanNFT.sol
@@ -6,8 +6,19 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NeapolitanNFT is ERC721 {
     uint256 private s_tokenCounter;
+    mapping(uint256 tokenId => string tokenUri) private s_tokenIdToUri;
 
     constructor() ERC721("Neapolitan Novels", "NNT") {
         s_tokenCounter = 0;
+    }
+
+    /**
+     * @dev mint an nft by passing the tokenUri yourself
+     * @param tokenUri a string which ideally should be an IPFS hash of a JSON object
+     */
+    function mintNft(string memory tokenUri) public {
+        s_tokenIdToUri[s_tokenCounter] = tokenUri;
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter = s_tokenCounter + 1;
     }
 }
