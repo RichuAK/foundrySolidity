2dd5fd6be9fdb58ea9e947b609f176e8b23a4cae
richuak <richuakuttikattu@gmail.com>
2023-09-12 12:08:14 +0530

    naples minting via interactions

--- nft/script/Interactions.s.sol
+++ nft/script/Interactions.s.sol
@@ -5,19 +5,24 @@ pragma solidity ^0.8.21;
 import {NeapolitanNFT} from "../src/NeapolitanNFT.sol";
 import {Script} from "forge-std/Script.sol";
 import {DeployNeapolitanNFT} from "./DeployNeapolitanNFT.s.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
 contract MintNapoliNFT is Script {
-    NeapolitanNFT neapolitanNFT;
-
     function run() external {
-        DeployNeapolitanNFT deployNFT = new DeployNeapolitanNFT();
-        vm.broadcast();
-        neapolitanNFT = deployNFT.run();
-        mintNFTFromDeployed();
+        // DeployNeapolitanNFT deployNFT = new DeployNeapolitanNFT();
+        // vm.startBroadcast();
+        // neapolitanNFT = deployNFT.run();
+        // vm.stopBroadcast();
+        address neapolitanNFTAddress = DevOpsTools.get_most_recent_deployment(
+            "NeapolitanNFT",
+            block.chainid
+        );
+        mintNFTFromDeployed(neapolitanNFTAddress);
     }
 
-    function mintNFTFromDeployed() internal {
-        vm.broadcast();
-        neapolitanNFT.mintNft("Some stupid hash of a junk picture");
+    function mintNFTFromDeployed(address NFTAddress) internal {
+        vm.startBroadcast();
+        NeapolitanNFT(NFTAddress).mintNft("Some stupid hash of a junk picture");
+        vm.stopBroadcast();
     }
 }
