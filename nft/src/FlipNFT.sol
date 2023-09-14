a7d41454b26e7b2625e6c7cb6af4f4677b8d0d01
richuak <richuakuttikattu@gmail.com>
2023-09-14 16:07:44 +0530

    Flip NFT init

---
+++ nft/src/FlipNFT.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.21;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract FlipNFT is ERC721 {
+    constructor() ERC721("Flip NFT", "FNT") {}
+}
