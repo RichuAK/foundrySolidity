0e292565fe704d204885f595adf5509f8954e315
richuak <richuakuttikattu@gmail.com>
2023-09-16 20:04:40 +0530

    FlipNFT interactions

--- nft/script/Interactions.s.sol
+++ nft/script/Interactions.s.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.21;
 
 import {NeapolitanNFT} from "../src/NeapolitanNFT.sol";
+import {FlipNFT} from "../src/FlipNFT.sol";
 import {Script} from "forge-std/Script.sol";
 import {DeployNeapolitanNFT} from "./DeployNeapolitanNFT.s.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
@@ -24,5 +25,44 @@ contract MintNapoliNFT is Script {
         vm.startBroadcast();
         NeapolitanNFT(NFTAddress).mintNft("Some stupid hash of a junk picture");
         vm.stopBroadcast();
+    }
+}
+
+contract MintFlipNFT is Script {
+    function run() external {
+        address flipNFTAddress = DevOpsTools.get_most_recent_deployment(
+            "FlipNFT",
+            block.chainid
+        );
+        mintFlipNFT(flipNFTAddress);
+    }
+
+    function mintFlipNFT(address flipNFTAddress) internal {
+        vm.broadcast();
+        FlipNFT(flipNFTAddress).mint();
+    }
+}
+
+contract FlipFlipNFT is Script {
+    // uint256 public s_tokenId;
+
+    // constructor(uint256 tokenId) {
+    //     s_tokenId = tokenId;
+    // }
+    // DOES THIS WORK?? IS THERE A WAY TO MAKE THIS WORK?
+
+    uint256 private constant STATIC_TOKEN_ID = 0;
+
+    function run() external {
+        address flipNFTAddress = DevOpsTools.get_most_recent_deployment(
+            "FlipNFT",
+            block.chainid
+        );
+        flipFlipNFT(flipNFTAddress);
+    }
+
+    function flipFlipNFT(address flipNFTAddress) internal {
+        vm.broadcast();
+        FlipNFT(flipNFTAddress).flipMood(STATIC_TOKEN_ID);
     }
 }
