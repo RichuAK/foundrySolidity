652831b805e8e30d9497f7d31b1e8c4c837e8d19
richuak <richuakuttikattu@gmail.com>
2023-09-17 14:23:53 +0530

    happy svg typo: should have tested

--- nft/script/DeployFlipNFT.s.sol
+++ nft/script/DeployFlipNFT.s.sol
@@ -27,7 +27,7 @@ contract DeployFlipNFT is Script {
 
         // reading from the file directly and then converting using OpenZeppelin's Base64, instead of passing the converted string
         string memory sadSvg = vm.readFile("./img/sad.svg");
-        string memory happySvg = vm.readFile("./img/sad.svg");
+        string memory happySvg = vm.readFile("./img/happy.svg");
         // string
         //     memory happySvgUri = "data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgMjAwIDIwMCIgd2lkdGg9IjQwMCIgIGhlaWdodD0iNDAwIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPgogIDxjaXJjbGUgY3g9IjEwMCIgY3k9IjEwMCIgZmlsbD0ieWVsbG93IiByPSI3OCIgc3Ryb2tlPSJibGFjayIgc3Ryb2tlLXdpZHRoPSIzIi8+CiAgPGcgY2xhc3M9ImV5ZXMiPgogICAgPGNpcmNsZSBjeD0iNjEiIGN5PSI4MiIgcj0iMTIiLz4KICAgIDxjaXJjbGUgY3g9IjEyNyIgY3k9IjgyIiByPSIxMiIvPgogIDwvZz4KICA8cGF0aCBkPSJtMTM2LjgxIDExNi41M2MuNjkgMjYuMTctNjQuMTEgNDItODEuNTItLjczIiBzdHlsZT0iZmlsbDpub25lOyBzdHJva2U6IGJsYWNrOyBzdHJva2Utd2lkdGg6IDM7Ii8+Cjwvc3ZnPg==";
         // string
