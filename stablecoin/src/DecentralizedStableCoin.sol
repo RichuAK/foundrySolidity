08c6286f7796091147b0918489c13c57725afb7b
richuak <richuakuttikattu@gmail.com>
2023-09-28 17:35:36 +0530

    burn function

--- stablecoin/src/DecentralizedStableCoin.sol
+++ stablecoin/src/DecentralizedStableCoin.sol
@@ -26,6 +26,14 @@
 pragma solidity 0.8.21;
 
 /**
+ * @notice Only importing ERC20Burnable instead of Both Burnable and ERC20 as in the course
+ * @notice Since Burnable is an extension that inherits from ERC20. Will see if works
+ * @notice Doesn't work, you need to import ERC20 for the constructor
+ */
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/Extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
  * @title DecentralizedStableCoin
  * @author Richu A Kuttikattu (not really, but okay)
  * @notice Meant to be governed by DSCEngine. This is just an ERC20 implementation of the stablecoin system
@@ -33,6 +41,26 @@ pragma solidity 0.8.21;
  * Minting: Algorithmic
  * Relative Stability: Pegged to USD
  */
-contract DecentralizedStableCoin {
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin__MustBeMoreThanZero();
+    error DecentralizedStableCoin__MustBeLessThanBalance();
 
+    constructor() ERC20("DecentralizedStableCoin", "DSC") {}
+
+    /**
+     *
+     * @param _amount amount to be burned
+     * @dev does a couple of checks and reverts or executes the original burn
+     */
+    function burn(uint256 _amount) public override onlyOwner {
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        if (balanceOf(msg.sender) < _amount) {
+            revert DecentralizedStableCoin__MustBeLessThanBalance();
+        }
+        // executes the burn function of the super class.
+        super.burn(_amount);
+        // the checks look a bit redundant to me, they're being performed in the original definition as well
+    }
 }
