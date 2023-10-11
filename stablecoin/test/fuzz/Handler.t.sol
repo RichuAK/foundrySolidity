77e184cf8053d730d144322b29d852da8083bc25
richuak <richuakuttikattu@gmail.com>
2023-10-11 17:52:37 +0530

    1 test via handler

--- stablecoin/test/fuzz/Handler.t.sol
+++ stablecoin/test/fuzz/Handler.t.sol
@@ -1,3 +1,39 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.21;
+
+import {Test} from "forge-std/Test.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+
+contract Handler is Test {
+    DecentralizedStableCoin dsc;
+    DSCEngine engine;
+    address weth;
+    address wbtc;
+
+    uint96 public constant MAX_DEPOSIT_SIZE = type(uint96).max;
+
+    constructor(DSCEngine _dscEngine, DecentralizedStableCoin _dsc) {
+        engine = _dscEngine;
+        dsc = _dsc;
+        address[] memory collateralTokens = engine.getCollateralTokens();
+        weth = collateralTokens[0];
+        wbtc = collateralTokens[1];
+    }
+
+    function depositCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
+        amountCollateral = bound(amountCollateral, 1, MAX_DEPOSIT_SIZE);
+
+        address collateral = _getCollateralFromSeed(collateralSeed);
+        engine.depositCollateral(collateral, amountCollateral);
+    }
+
+    function _getCollateralFromSeed(uint256 collateralSeed) private view returns (address) {
+        if (collateralSeed % 2 == 0) {
+            return weth;
+        } else {
+            return wbtc;
+        }
+    }
+}
--- stablecoin/test/fuzz/InvariantsTest.t.sol
+++ stablecoin/test/fuzz/InvariantsTest.t.sol
@@ -6,3 +6,44 @@ pragma solidity ^0.8.21;
 
 // 1. The debt should never exceed the collateral. i.e DSCTotalSupply < TotalCollateralValueInUSD
 // 2. Getter view functions should never revert (evergreen invariant).
+
+import {Test, console} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
+import {Handler} from "./Handler.t.sol";
+
+contract InvariantTest is StdInvariant, Test {
+    DeployDSC deployer;
+    HelperConfig helperConfig;
+    DecentralizedStableCoin dsc;
+    DSCEngine engine;
+    Handler handler;
+    address weth;
+    address wbtc;
+
+    function setUp() external {
+        deployer = new DeployDSC();
+        (dsc, engine, helperConfig) = deployer.run();
+        (,, weth, wbtc,) = helperConfig.activeNetworkConfig();
+        handler = new Handler(engine, dsc);
+        targetContract(address(handler));
+    }
+
+    function invariant_protocolMustHaveMoreValueThatTotalSupplyDollars() public view {
+        uint256 totalSupply = dsc.totalSupply();
+        uint256 wethDeposted = ERC20Mock(weth).balanceOf(address(engine));
+        uint256 wbtcDeposited = ERC20Mock(wbtc).balanceOf(address(engine));
+
+        uint256 wethValue = engine.getUsdValue(weth, wethDeposted);
+        uint256 wbtcValue = engine.getUsdValue(wbtc, wbtcDeposited);
+
+        console.log("wethValue: %s", wethValue);
+        console.log("wbtcValue: %s", wbtcValue);
+
+        assert(wethValue + wbtcValue >= totalSupply);
+    }
+}
