e45810d6bda08a21f21285c48964e32f3df79115
richuak <richuakuttikattu@gmail.com>
2023-10-13 17:31:21 +0530

    mint() with deposit() inside it

--- stablecoin/test/fuzz/Handler.t.sol
+++ stablecoin/test/fuzz/Handler.t.sol
@@ -12,6 +12,8 @@ contract Handler is Test {
     DSCEngine engine;
     address weth;
     address wbtc;
+    uint256 public mintCount;
+    uint256 public redeemCount;
 
     uint96 public constant MAX_DEPOSIT_SIZE = type(uint96).max;
 
@@ -31,6 +33,34 @@ contract Handler is Test {
         ERC20Mock(collateral).mint(msg.sender, amountCollateral);
         ERC20Mock(collateral).approve(address(engine), amountCollateral);
         engine.depositCollateral(collateral, amountCollateral);
+        vm.stopPrank();
+    }
+
+    function mint(uint256 amount, uint256 collateralSeed, uint256 amountCollateral) public {
+        depositCollateral(collateralSeed, amountCollateral);
+        (uint256 totalDscMinted, uint256 totalCollateralValueInUsd) = engine.getUserInformation(msg.sender);
+        int256 maxDscToMint = int256((totalCollateralValueInUsd / 2) - totalDscMinted);
+        if (maxDscToMint < 0) {
+            return;
+        }
+        amount = bound(amount, 1, uint256(maxDscToMint));
+        vm.prank(msg.sender);
+        engine.mintDsc(amount);
+        mintCount++;
+    }
+
+    function redeemCollateral(uint256 collateralSeed, uint256 collateralAmount) public {
+        // depositCollateral(collateralSeed, collateralAmount);
+        address collateral = _getCollateralFromSeed(collateralSeed);
+        vm.startPrank(msg.sender);
+        uint256 maxCollateralToRedeem = engine.getCollateralBalanceOfUser(msg.sender, collateral);
+        collateralAmount = bound(collateralAmount, 0, maxCollateralToRedeem);
+        if (collateralAmount == 0) {
+            return;
+        }
+        engine.redeemCollateral(collateral, collateralAmount);
+        vm.stopPrank();
+        redeemCount++;
     }
 
     function _getCollateralFromSeed(uint256 collateralSeed) private view returns (address) {
