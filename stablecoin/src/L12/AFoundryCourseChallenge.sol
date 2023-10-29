32452a3a492f975e4830882d03cc3e4f6567d76b
richuak <richuakuttikattu@gmail.com>
2023-10-29 09:21:26 +0530

    Lesson12

---
+++ stablecoin/src/L12/AFoundryCourseChallenge.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract AFoundryCourseChallenge {
+    constructor(address FoundryCourseNftNft) {}
+    function description() external view returns (string memory) {}
+
+    function extraDescription(address user) external view returns (string memory) {}
+
+    function specialImage() external view returns (string memory) {}
+
+    function attribute() external view returns (string memory) {}
+
+    function _updateAndRewardSolver(string memory twitterHandleOfSolver) internal {}
+
+    /* Each contract must have a "solveChallenge" function, however, the signature
+     * maybe be different in all cases because of different input parameters.
+     * Because of this, we are not going to define the function here.
+     *
+     * This function should call back to the FoundryCourseNft contract
+     * to mint the NFT.
+     */
+    // function solveChallenge() external;
+}
